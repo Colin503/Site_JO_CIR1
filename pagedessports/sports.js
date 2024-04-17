f8e5ece89339de057dab249838f39d6b5c7333ec
rivtoc <165174386+rivtoc@users.noreply.github.com>
2024-04-17 15:41:14 +0200

    Add files via upload

---
+++ pagedessports/sports.js
@@ -0,0 +1,9 @@
+
+document.getElementById("badminton").onmouseover = function()
+{
+    document.getElementById('image').src="dessinbadmintonpagesport.png";
+}
+document.getElementById("badminton").onmouseout = function()
+{
+    document.getElementById('image').style.display="none";
+}
