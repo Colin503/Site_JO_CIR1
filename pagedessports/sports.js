0df4e6dbed65ceb7fe027558c7ac4240e9274785
Colin503 <86229895+Colin503@users.noreply.github.com>
2024-05-15 15:43:36 +0200

    Delete pagedessports directory

--- pagedessports/sports.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("badminton").onmouseover = function()
-{
-    document.getElementById('image').src="dessinbadmintonpagesport.png";
-}
-document.getElementById("badminton").onmouseout = function()
-{
-    document.getElementById('image').style.display="none";
-}
